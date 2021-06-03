2664525c134f575c25c453cad45012ca1c0b4415
halx99 <halx99@live.com>
2021-06-03 17:07:31 +0800

    Fixup

--- 1k/build.sh
+++ 1k/build.sh
@@ -9,40 +9,45 @@ android_api_level=$(cat source.properties | grep -w 'android_api_level' | cut -d
 android_api_level_arm64=$(cat source.properties | grep -w 'android_api_level_arm64' | cut -d '=' -f 2 | tr -d ' \n')
 
 echo "RUNNER_OS=$RUNNER_OS"
+echo "BUILD_TARGET=$BUILD_TARGET"
+echo "BUILD_ARCH=$BUILD_ARCH"
 
 # Determine build target & config options
 OPENSSL_CONFIG_OPTIONS=$openssl_config_options_1
 if [ "$BUILD_TARGET" = "linux" ]; then
-    OPENSSL_TARGET=
+    OPENSSL_CONFIG_TARGET=
 elif [ "$BUILD_TARGET" = "osx" ]; then
-    OPENSSL_TARGET=darwin64-x86_64-cc
+    OPENSSL_CONFIG_TARGET=darwin64-x86_64-cc
 elif [ "$BUILD_TARGET" = "ios" ]; then
     IOS_PLATFORM=OS
     if [ "$BUILD_ARCH" = "arm" ] ; then
-        OPENSSL_TARGET=ios-cross
+        OPENSSL_CONFIG_TARGET=ios-cross
     elif [ "$BUILD_ARCH" = 'arm64' ] ; then
-        OPENSSL_TARGET=ios64-cross
+        OPENSSL_CONFIG_TARGET=ios64-cross
     elif [ "$BUILD_ARCH" = "x86_x64" ] ; then
-        OPENSSL_TARGET=darwin64-x86_64-cc
+        OPENSSL_CONFIG_TARGET=darwin64-x86_64-cc
         IOS_PLATFORM=Simulator
     fi
-    OPENSSL_CONFIG_OPTIONS=$OPENSSL_CONFIG_OPTIONS $openssl_config_options_2
+    OPENSSL_CONFIG_OPTIONS="$OPENSSL_CONFIG_OPTIONS $openssl_config_options_2"
     export CROSS_TOP=$(xcode-select -print-path)/Platforms/iPhone${IOS_PLATFORM}.platform/Developer
     export CROSS_SDK=iPhone${IOS_PLATFORM}.sdk
 elif [ "$BUILD_TARGET" = "android" ] ; then
-    wget https://dl.google.com/android/repository/android-ndk-${NDK_VER}-linux-x86_64.zip
-    unzip -q android-ndk-${NDK_VER}-linux-x86_64.zip
-    export ANDROID_NDK_HOME=`pwd`/android-ndk-${NDK_VER}
+    wget https://dl.google.com/android/repository/android-ndk-${ndk_ver}-linux-x86_64.zip
+    unzip -q android-ndk-${ndk_ver}-linux-x86_64.zip
+    export ANDROID_NDK_HOME=`pwd`/android-ndk-${ndk_ver}
     export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
     if [ "$BUILD_ARCH" = "arm64" ] ; then
-        OPENSSL_TARGET=android-$BUILD_ARCH -D__ANDROID_API__=$android_api_level_arm64
+        OPENSSL_CONFIG_TARGET="android-$BUILD_ARCH -D__ANDROID_API__=$android_api_level_arm64"
     else
-        OPENSSL_TARGET=android-$BUILD_ARCH -D__ANDROID_API__=$android_api_level
+        OPENSSL_CONFIG_TARGET="android-$BUILD_ARCH -D__ANDROID_API__=$android_api_level"
     fi
-    OPENSSL_CONFIG_OPTIONS=$OPENSSL_CONFIG_OPTIONS $openssl_config_options_2
+    OPENSSL_CONFIG_OPTIONS="$OPENSSL_CONFIG_OPTIONS $openssl_config_options_2"
 else
-  exit 0
+  return 0
 fi
+
+echo OPENSSL_CONFIG_TARGET=${OPENSSL_CONFIG_TARGET}
+echo OPENSSL_CONFIG_OPTIONS=${OPENSSL_CONFIG_OPTIONS}
 
 # Checkout openssl
 git clone https://github.com/openssl/openssl.git
@@ -56,11 +61,13 @@ openssl_src_root=`pwd`
 INSTALL_NAME=${BUILD_TARGET}_${BUILD_ARCH}
 openssl_install_dir=$openssl_src_root/$INSTALL_NAME
 mkdir $openssl_install_dir
-echo $OPENSSL_TARGET $OPENSSL_CONFIG_OPTIONS --prefix=$openssl_install_dir --openssldir=$openssl_install_dir
+echo $OPENSSL_CONFIG_TARGET $OPENSSL_CONFIG_OPTIONS --prefix=$openssl_install_dir --openssldir=$openssl_install_dir
+OPENSSL_CONFIG_ALL_OPTIONS="OPENSSL_CONFIG_TARGET $OPENSSL_CONFIG_OPTIONS --prefix=$openssl_install_dir --openssldir=$openssl_install_dir"
+echo OPENSSL_CONFIG_ALL_OPTIONS=${OPENSSL_CONFIG_ALL_OPTIONS}
 if [ "$RUNNER_OS" = "Linux" ] ; then
-    ./config $OPENSSL_TARGET $OPENSSL_CONFIG_OPTIONS --prefix=$openssl_install_dir --openssldir=$openssl_install_dir && perl configdata.pm --dump
+    ./config $OPENSSL_CONFIG_ALL_OPTIONS && perl configdata.pm --dump
 else
-    ./Configure $OPENSSL_TARGET $OPENSSL_CONFIG_OPTIONS --prefix=$openssl_install_dir --openssldir=$openssl_install_dir && perl configdata.pm --dump
+    ./Configure $OPENSSL_CONFIG_ALL_OPTIONS && perl configdata.pm --dump
 fi
 # make VERBOSE=1
 # make install
