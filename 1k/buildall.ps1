f4cac8c5d7cfb95d47bd3deb0e32fad86da58d0e
halx99 <halx99@live.com>
2021-06-07 18:47:35 +0800

    Draft: update

--- 1k/buildall.ps1
+++ 1k/buildall.ps1
@@ -5,7 +5,7 @@ $build_script = "$buildware_root\1k\build.ps1"
 $INSTALL_ROOT="install_windows_${ARCH}"
 
 Invoke-Expression -Command "$build_script jpeg-turbo $ARCH $INSTALL_ROOT"
-# Invoke-Expression -Command "$build_script openssl $ARCH $INSTALL_ROOT"
+Invoke-Expression -Command "$build_script openssl $ARCH $INSTALL_ROOT"
 
 # Export INSTALL_ROOT for uploading
 Write-Output "INSTALL_ROOT=$INSTALL_ROOT" >> ${env:GITHUB_ENV}
