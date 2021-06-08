02e15dfa17e71cbc23ad1fb83cae3b6c68976eae
halx99 <halx99@live.com>
2021-06-08 10:40:57 +0800

    Check openssl bin

--- src/openssl/clean.ps1
+++ src/openssl/clean.ps1
@@ -5,7 +5,9 @@ if ((Test-Path $install_dir -PathType Container)) {
     # Delete files what we don't want
     Remove-Item "$install_dir\html" -recurse
     Remove-Item "$install_dir\lib\engines-1_1" -recurse
+    ls -R "$install_dir\bin"
     Remove-Item "$install_dir\bin\*.pl"
     Remove-Item "$install_dir\bin\*.pdb"
     Remove-Item "$install_dir\bin\*.exe"
+    ls -R "$install_dir\bin"
 }
