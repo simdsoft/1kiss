e51dbe7c0446a78fb85687b65b6fa4f097a5f2a9
halx99 <halx99@live.com>
2021-08-26 14:01:41 +0800

    dist luajit

--- src/luajit/dist1.sh
+++ src/luajit/dist1.sh
@@ -1,13 +1,12 @@
-# DIST_ROOT=$1
-# LIB_NAME=openssl
-# DIST_DIR="${DIST_ROOT}/${LIB_NAME}"
+DIST_ROOT=$1
+LIB_NAME=luajit
+DIST_DIR="${DIST_ROOT}/${LIB_NAME}"
 
-# copy_inc_and_libs ${LIB_NAME} ${DIST_DIR} opensslconf.h config.h.in openssl/
+copy_inc_and_libs ${LIB_NAME} ${DIST_DIR}
 
-# # create flat lib for ios
-# lipo -create install_ios_arm/${LIB_NAME}/lib/libssl.a install_ios_arm64/${LIB_NAME}/lib/libssl.a install_ios_x64/${LIB_NAME}/lib/libssl.a -output ${DIST_DIR}/prebuilt/ios/libssl.a
-# lipo -create install_ios_arm/${LIB_NAME}/lib/libcrypto.a install_ios_arm64/${LIB_NAME}/lib/libcrypto.a install_ios_x64/${LIB_NAME}/lib/libcrypto.a -output ${DIST_DIR}/prebuilt/ios/libcrypto.a
+# create flat lib for ios
+# lipo -create install_ios_arm/${LIB_NAME}/lib/libluajit.a install_ios_arm64/${LIB_NAME}/lib/libluajit.a install_ios_x64/${LIB_NAME}/lib/libluajit.a -output ${DIST_DIR}/prebuilt/ios/libluajit.a
+lipo -create install_ios_arm64/${LIB_NAME}/lib/libluajit.a install_ios_x64/${LIB_NAME}/lib/libluajit.a -output ${DIST_DIR}/prebuilt/ios/libluajit.a
 
-# # check the flat lib
-# lipo -info ${DIST_DIR}/prebuilt/ios/libssl.a
-# lipo -info ${DIST_DIR}/prebuilt/ios/libcrypto.a
+# check the flat lib
+lipo -info ${DIST_DIR}/prebuilt/ios/libluajit.a
