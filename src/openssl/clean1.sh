f777d82d1e11bc80d234e63748c0a172666782b0
halx99 <halx99@live.com>
2021-09-21 22:58:02 +0800

    Update clean1.sh

--- src/openssl/clean1.sh
+++ src/openssl/clean1.sh
@@ -5,4 +5,11 @@ if [ -d "$install_dir" ] ; then
     rm -rf $install_dir/bin
     rm -rf $install_dir/misc
     rm -rf $install_dir/share
+    if [ "$BUILD_TARGET" = "linux" ] ; then
+        mv $install_dir/lib64 $install_dir/lib
+    fi
+
+    if [ "$BUILD_TARGET" = "linux" ] || [ "$BUILD_TARGET" = "osx" ] ; then
+        rm -rf $install_dir/lib/ossl-modules
+    fi
 fi
