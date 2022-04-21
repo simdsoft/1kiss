13a6cdb27fed167274891d01d341f7602379307d
halx99 <halx99@live.com>
2022-04-21 14:07:36 +0800

    Test zlib

--- src/zlib/clean1.sh
+++ src/zlib/clean1.sh
@@ -3,7 +3,7 @@ install_dir=$1
 if [ -d "$install_dir" ] ; then
     echo "Cleaning ${install_dir}..."
     # Delete files what we don't want
-    # rm -rf "$install_dir/share"
+    rm -rf "$install_dir/share"
     # rm -rf "$install_dir/lib/cmake"
     # rm -rf "$install_dir/lib/pkgconfig"
 fi
