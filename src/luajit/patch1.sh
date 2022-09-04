e56c89f53815f22b1cebedad1e00776b4dfdbced
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-09-03 23:31:33 -0300

    tvos support

--- src/luajit/patch1.sh
+++ src/luajit/patch1.sh
@@ -1,8 +1,31 @@
-
 if [[ ! "$ndk_ver" == "" ]] && [[ $ndk_ver > '21' ]] ; then
     script_dir=$1
     lib_src_dir=$2
     echo "LuaJIT: patching makefile for build on ndk-r22+ ..."
 
-    cp -f $script_dir/Makefile $lib_src_dir/src/
+    cp -f $script_dir/ndk/Makefile $lib_src_dir/src/
+fi
+
+if [[ "$BUILD_TARGET" == "ios" ]] ; then
+    script_dir=$1
+    lib_src_dir=$2
+    echo "LuaJIT: patching makefile for build on iOS ..."
+
+    cp -f $script_dir/apple/Makefile $lib_src_dir/src/
+fi
+
+if [[ "$BUILD_TARGET" == "tvos" ]] ; then
+    script_dir=$1
+    lib_src_dir=$2
+    echo "LuaJIT: patching makefile for build on tvOS ..."
+
+    cp -f $script_dir/apple/Makefile $lib_src_dir/src/
+fi
+
+if [[ "$BUILD_TARGET" == "watchos" ]] ; then
+    script_dir=$1
+    lib_src_dir=$2
+    echo "LuaJIT: patching makefile for build on watchOS ..."
+
+    cp -f $script_dir/apple/Makefile $lib_src_dir/src/
 fi
