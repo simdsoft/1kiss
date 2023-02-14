db7d97f62b76ff483a8bd98e4727e58c4bd63c39
Deal(一线灵) <halx99@live.com>
2023-02-14 14:49:20 +0800

    Remove llvm from build [skip ci]

--- 1k/build.sh
+++ 1k/build.sh
@@ -89,7 +89,7 @@ fi
 
 # Build libs
 if [ "$BUILD_LIBS" = "" ] ; then
-   BUILD_LIBS="zlib,openssl,curl,jpeg-turbo,glsl-optimizer,luajit,llvm"
+   BUILD_LIBS="zlib,openssl,curl,jpeg-turbo,glsl-optimizer,luajit"
 fi
 
 libs_arr=(${BUILD_LIBS//,/ })
