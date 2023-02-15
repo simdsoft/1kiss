6c85026c26785573df849b8c8cc6404b43426214
Deal(一线灵) <halx99@live.com>
2023-02-15 14:09:36 +0800

    Disable soversion [skip ci]

--- src/llvm/clean1.sh
+++
@@ -1,8 +0,0 @@
-install_dir=$1
-
-if [ -d "$install_dir" ] ; then
-    echo "Cleaning ${install_dir}..."
-    if [ "$BUILD_TARGET" = "linux" ] ; then
-        rm -f $install_dir/lib/*.so.*
-    fi
-fi
