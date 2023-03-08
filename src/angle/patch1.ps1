994990b949c8438220adede3cc5b2672b9f4decb
halx99 <halx99@live.com>
2023-03-08 20:00:42 +0800

    Fix ci

--- src/angle/patch1.ps1
+++ src/angle/patch1.ps1
@@ -1,3 +1,4 @@
 $LIB_ROOT = $args[0]
 
 Copy-Item -Path "$PSScriptRoot\build\*" -Destination "$LIB_ROOT\build" -Recurse -Force
+Copy-Item -Path "$PSScriptRoot\src\*" -Destination "$LIB_ROOT\src" -Recurse -Force
