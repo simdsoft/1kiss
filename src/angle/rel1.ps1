88227ac8d3863980a2227de68dbf1453198c5782
halx99 <halx99@live.com>
2023-03-22 20:37:58 +0800

    Fix fetch chrome release

--- src/angle/rel1.ps1
+++ src/angle/rel1.ps1
@@ -1,5 +1,13 @@
 $LIB_ROOT = $args[0]
-$chromeRelStr = $(curl -L "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num=1")
-$chromeRelInfo = $(ConvertFrom-Json -InputObject "$chromeRelStr" -AsHashtable)
-$chromeVersion = $chromeRelInfo.version
-Out-File -FilePath $LIB_ROOT\bw_version.yml -InputObject "bw_version: $chromeVersion" -Encoding ASCII -Append
+$chromeRelStr = $(curl -L "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows")
+$chromeRelList = $(ConvertFrom-Json -InputObject "$chromeRelStr" -AsHashtable)
+
+$chromeLatest = "0.0.0.0"
+foreach($relInfo in $chromeRelList) {
+    $ver = $relInfo['version']
+    if ([System.Version]$ver -gt [System.Version]$chromeLatest) {
+        $chromeLatest = $ver
+    }
+}
+
+Out-File -FilePath $LIB_ROOT\bw_version.yml -InputObject "bw_version: $chromeLatest" -Encoding ASCII -Append
