b05a3d54a1e21f5610816b14baab357397a14b4c
halx99 <halx99@live.com>
2023-06-10 23:48:15 +0800

    No longer build windows x86 prebuilts

--- 1k/dist.sh
+++ 1k/dist.sh
@@ -70,8 +70,7 @@ function dist_lib {
 
         # mkdir for platform spec config header file
         if [ "$CONF_TEMPLATE" = "config.h.in" ] ; then
-            mkdir -p ${DIST_DIR}/include/win32/${INC_DIR}
-            mkdir -p ${DIST_DIR}/include/win64/${INC_DIR}
+            mkdir -p ${DIST_DIR}/include/win/${INC_DIR}
             mkdir -p ${DIST_DIR}/include/linux/${INC_DIR}
             mkdir -p ${DIST_DIR}/include/mac/${INC_DIR}
             # mkdir -p ${DIST_DIR}/include/ios-arm/${INC_DIR}
@@ -84,7 +83,7 @@ function dist_lib {
             mkdir -p ${DIST_DIR}/include/android-x86/${INC_DIR}
             mkdir -p ${DIST_DIR}/include/android-x86_64/${INC_DIR}
         elif [ "$CONF_TEMPLATE" = "config_ab.h.in" ] ; then
-            mkdir -p ${DIST_DIR}/include/win32/${INC_DIR}
+            mkdir -p ${DIST_DIR}/include/win/${INC_DIR}
             mkdir -p ${DIST_DIR}/include/unix/${INC_DIR}
         fi
 
@@ -107,8 +106,8 @@ function dist_lib {
 
             # copy platform spec config header file
             if [ "$CONF_TEMPLATE" = "config.h.in" ] ; then
-                cp install_win_x86/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/win32/${INC_DIR}
-                cp install_win_x64/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/win64/${INC_DIR}
+                # cp install_win_x86/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/win32/${INC_DIR}
+                cp install_win_x64/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/win/${INC_DIR}
                 cp install_linux_x64/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/linux/${INC_DIR}
                 cp install_osx_x64/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/mac/${INC_DIR}
                 # cp install_ios_arm/${LIB_NAME}/include/${INC_DIR}${CONF_HEADER} ${DIST_DIR}/include/ios-arm/${INC_DIR}
@@ -130,13 +129,13 @@ function dist_lib {
 
     # create prebuilt dirs
     if [ ! $(($DIST_FLAGS & $DISTF_WIN)) = 0 ]; then
-        mkdir -p ${DIST_DIR}/prebuilt/win/x86
-        copy1k "install_win_x86/${LIB_NAME}/lib/*.lib" ${DIST_DIR}/prebuilt/win/x86/
-        copy1k "install_win_x86/${LIB_NAME}/bin/*.dll" ${DIST_DIR}/prebuilt/win/x86/
+        # mkdir -p ${DIST_DIR}/prebuilt/win/x86
+        # copy1k "install_win_x86/${LIB_NAME}/lib/*.lib" ${DIST_DIR}/prebuilt/win/x86/
+        # copy1k "install_win_x86/${LIB_NAME}/bin/*.dll" ${DIST_DIR}/prebuilt/win/x86/
 
-        mkdir -p ${DIST_DIR}/prebuilt/win/x64
-        copy1k "install_win_x64/${LIB_NAME}/lib/*.lib" ${DIST_DIR}/prebuilt/win/x64/
-        copy1k "install_win_x64/${LIB_NAME}/bin/*.dll" ${DIST_DIR}/prebuilt/win/x64/
+        mkdir -p ${DIST_DIR}/prebuilt/win/
+        copy1k "install_win_x64/${LIB_NAME}/lib/*.lib" ${DIST_DIR}/prebuilt/win/
+        copy1k "install_win_x64/${LIB_NAME}/bin/*.dll" ${DIST_DIR}/prebuilt/win/
     fi
 
     if [ ! $(($DIST_FLAGS & $DISTF_UWP)) = 0 ]; then
--- README.md
+++ README.md
@@ -38,7 +38,6 @@ https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&
   - x86
   - x86_64
 - win (Windows Desktop Apps)
-  - x86
   - x86_64
 - uwp (Windows Universal Apps)
   - x86_64
