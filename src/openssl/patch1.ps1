5b51a9c12becca816cae71ae15e08b5fb4145917
halx99 <halx99@live.com>
2023-06-10 23:52:15 +0800

    Update openssl to 3.0.9

--- src/openssl/patch1.ps1
+++
@@ -1,5 +0,0 @@
-$LIB_ROOT = $args[0]
-
-# since 3.1.0, the patch source is identical
-# since 3.0.9/3.1.1, the rsa_sup_mul.c was removed
-Copy-Item "$PSScriptRoot\rsa_sup_mul.c" "$LIB_ROOT\crypto\bn\rsa_sup_mul.c" -Force
