40fd2c13df4cf01b9285d4af03d9fa2862dd9de6
halx99 <halx99@live.com>
2024-04-02 22:25:48 +0800

    Fixup [skip ci]

--- 1k/build.ps1
+++ 1k/build.ps1
@@ -230,8 +230,7 @@ $options = @{
     xc     = @()
     xb     = @()
     j      = -1
-    sdk    = $null
-    env    = ''
+    sdk    = ''
     dll    = $false
 }
 
@@ -1765,7 +1764,7 @@ if (!$setupOnly) {
         }
         elseif ($Global:is_ios) {
             $gn_buildargs_overrides += 'target_os=\"ios\"'
-            if ($TARGET_CPU -eq 'x64' -or $Global:is_ios_sim) {
+            if ($Global:is_ios_sim) {
                 $gn_buildargs_overrides += 'target_environment=\"simulator\"'
             }
         }
