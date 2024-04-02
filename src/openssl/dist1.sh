896db394c831d8e2572fd09dcd9fe79bf44d86cd
halx99 <halx99@live.com>
2024-04-02 23:45:39 +0800

    Fixup[skip ci]

--- src/openssl/dist1.sh
+++ src/openssl/dist1.sh
@@ -4,18 +4,20 @@ DIST_DIR="${DIST_ROOT}/${LIB_NAME}"
 
 dist_lib ${LIB_NAME} ${DIST_DIR} $DISTF_ALL configuration.h config.h.in openssl/
 
-create_fat ${LIB_NAME} libssl.a
-create_fat ${LIB_NAME} libcrypto.a
+function combine_openssl {
+  dir=$1
+  libtool -static -o $dir/${LIB_NAME}/lib/libopenssl.a \
+    $dir/${LIB_NAME}/lib/libcrypto.a \
+    $dir/${LIB_NAME}/lib/libssl.a
+}
 
-xcodebuild -create-xcframework \
-    -library install_ios_arm64/${LIB_NAME}/lib/libssl.a \
-    -library fat_tmp/${LIB_NAME}/lib/ios_sim/libssl.a \
-    -library install_tvos_arm64/${LIB_NAME}/lib/libssl.a \
-    -library fat_tmp/${LIB_NAME}/lib/tvos_sim/libssl.a \
-    -library fat_tmp/${LIB_NAME}/lib/mac/libssl.a \
-    -library install_ios_arm64/${LIB_NAME}/lib/libcrypto.a \
-    -library fat_tmp/${LIB_NAME}/lib/ios_sim/libcrypto.a \
-    -library install_tvos_arm64/${LIB_NAME}/lib/libcrypto.a \
-    -library fat_tmp/${LIB_NAME}/lib/tvos_sim/libcrypto.a \
-    -library fat_tmp/${LIB_NAME}/lib/mac/libcrypto.a \
-    -output ${DIST_DIR}/lib/ios/$NAME.xcframework
+combine_openssl install_ios_arm64
+combine_openssl install_ios_x64
+combine_openssl install_ios_arm64_sim
+combine_openssl install_tvos_arm64
+combine_openssl install_tvos_x64
+combine_openssl install_tvos_arm64_sim
+combine_openssl install_osx_x64
+combine_openssl install_osx_arm64
+
+create_xcfraemwork openssl ${LIB_NAME} libopenssl.a
