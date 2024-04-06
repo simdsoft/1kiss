334b29f8c77b3a55a9c02f56039c9dc8c08f664b
halx99 <halx99@live.com>
2024-04-06 15:41:28 +0800

    Improve script [skip ci]

---
+++ 1k/install-cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cmake_ver=$1
+if [ "$cmake_ver" = "" ] ; then
+    cmake_ver='3.29.1'
+fi
+
+myRoot=$(dirname "$0")
+cacheDir=$myRoot/.cache
+mkdir -p $cacheDir
+
+install_script=$cacheDir/cmake-${cmake_ver}-linux-x86_64.sh
+
+if [ ! -f "${install_script}" ] ; then
+    wget https://github.com/Kitware/CMake/releases/download/v${cmake_ver}/cmake-${cmake_ver}-linux-x86_64.sh -O $install_script
+fi
+
+chmod u+x ${install_script}
+${install_script} --skip-license --prefix=/usr/local
--- 1k/install-pwsh.sh
+++ 1k/install-pwsh.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Install powershell 7 on macOS, Ubuntu, ArchLinux
 # usage: ./install-pwsh [pwsh_ver]
 #
