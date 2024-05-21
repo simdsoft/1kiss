6d3be1cc72f321bfea7d2b3055eb85e7f9ed7744
halx99 <halx99@live.com>
2024-05-22 02:31:53 +0800

    Update dist1.sh [skip ci]

--- src/llvm/dist1.sh
+++ src/llvm/dist1.sh
@@ -9,4 +9,5 @@ mkdir -p fat_tmp/${LIB_NAME}/lib/mac/
 lipo -create install_osx_arm64/${LIB_NAME}/lib/$LIB_FILE install_osx_x64/${LIB_NAME}/lib/$LIB_FILE -output fat_tmp/${LIB_NAME}/lib/mac/$LIB_FILE
 lipo -info fat_tmp/${LIB_NAME}/lib/mac/$LIB_FILE
 
+mkdir -p ${DIST_DIR}/lib/mac/
 copy1k fat_tmp/${LIB_NAME}/mac/$LIB_FILE ${DIST_DIR}/lib/mac/
