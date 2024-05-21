91d8419d120cde4c11d37324a88084f335588bc0
halx99 <halx99@live.com>
2024-05-22 02:27:25 +0800

    Update dist1.sh [skip ci]

--- src/llvm/dist1.sh
+++ src/llvm/dist1.sh
@@ -4,7 +4,7 @@ DIST_DIR="${DIST_ROOT}/${LIB_NAME}"
 
 dist_lib ${LIB_NAME} ${DIST_DIR} $(($DISTF_WIN32|$DISTF_LINUX|$DISTF_MAC|$DISTF_NO_INC))
 
-$LIB_FILE = 'libclang.dylib'
+LIB_FILE=libclang.dylib
 mkdir -p fat_tmp/${LIB_NAME}/lib/mac/
 lipo -create install_osx_arm64/${LIB_NAME}/lib/$LIB_FILE install_osx_x64/${LIB_NAME}/lib/$LIB_FILE -output fat_tmp/${LIB_NAME}/lib/mac/$LIB_FILE
 lipo -info fat_tmp/${LIB_NAME}/lib/mac/$LIB_FILE
