9b39828f16be1d68f3e00f787722da25933247c7
halx99 <halx99@live.com>
2026-07-12 22:40:47 +0800

    Fix dxc dist [skip ci]

--- src/dxc/dist1.sh
+++ src/dxc/dist1.sh
@@ -2,7 +2,7 @@ DIST_ROOT=$1
 LIB_NAME=dxc
 DIST_DIR="${DIST_ROOT}/${LIB_NAME}"
 
-dist_lib ${LIB_NAME} ${DIST_DIR} $(($DISTF_WIN32|$DISTF_LINUX|$DISTF_MAC))
+dist_lib ${LIB_NAME} ${DIST_DIR} $(($DISTF_WIN32|$DISTF_LINUX))
 
 if [ -d "install_osx_arm64/${LIB_NAME}/lib" ] && [ -d "install_osx_x64/${LIB_NAME}/lib" ]; then
     LIB_FILE=libdxcompiler.dylib
