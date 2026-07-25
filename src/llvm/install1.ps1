dfb55e422c8dbbb652a9863cb7ea9d7fe2551142
halx99 <halx99@live.com>
2026-07-25 22:29:49 +0800

    Update llvm install1.ps1 [skip ci]

--- src/llvm/install1.ps1
+++ src/llvm/install1.ps1
@@ -1,12 +1,24 @@
 $install_dir = $args[0]
 
-$artifact_files = @(@('Release\bin\libclang.dll', 'Release\bin\clang-format.exe'), 'lib\libclang.so', 'Release\lib\libclang.dylib')[$HOST_OS_INT]
-$install_dest = (Join-Path $install_dir (@('lib', 'bin')[$IsWin]))
-mkdirs $install_dest
+$artifact_files = @(
+    @('Release\bin\libclang.dll', 'Release\bin\clang-format.exe'),
+    @('lib/libclang.so', 'bin/clang-format'),
+    @('Release/lib/libclang.dylib', 'Release/bin/clang-format')
+)[$HOST_OS_INT]
 
 foreach($path in $artifact_files) {
-    $full_path = (Join-Path $BUILD_DIR "$path")
+    $full_path = Join-Path $BUILD_DIR $path
     if (Test-Path $full_path -PathType Leaf) {
+        $ext = [System.IO.Path]::GetExtension($path)
+        if ($ext -in '.dll', '.exe') {
+            $sub_dir = 'bin'
+        } elseif ($ext -in '.dylib', '.so') {
+            $sub_dir = 'lib'
+        } else {
+            $sub_dir = 'bin'
+        }
+        $install_dest = Join-Path $install_dir $sub_dir
+        mkdirs $install_dest
         Write-Host "Copying $full_path to $install_dest"
         Copy-Item $full_path $install_dest
     } else {
